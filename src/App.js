83f7ec406a71e4b4bff5e1e2878f3762f02e2497
nisalnmhj <nisaln.maharzan@gmail.com>
2024-03-08 13:23:26 -0800

    My bot

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import ChatBot from 'react-simple-chatbot';
 
 function App() {
+    const config = {
+      botAvatar: './logo.svg',
+      floating: true,
+    };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ChatBot
+        steps={[
+               {
+                 id: '1',
+                  message: 'Hello I am Nishan porfolio chatbot!',
+                  trigger: '2',
+                },
+                {
+                  id: '2',
+                   message: 'I can give following information about him',
+                   trigger: '3',
+                 },
+                {
+                  id: '3',
+                  options: [
+                    { value: 1, label: 'Intro', trigger: '4' },
+                    { value: 2, label: 'school', trigger: '5' },
+                    { value: 3, label: 'end', trigger: '6' },
+                  ],
+                },
+                {
+                  id: '4',
+                  message: 'He is a computer science student',
+                  trigger: '3',
+                },
+                {
+                  id: '5',
+                  message: 'He is studying at sac state',
+                  trigger: '3',
+                },
+                {
+                  id: '6',
+                  message: 'Good Bye!',
+                  end: true,
+                },
+  ]} 
+  config = {config}
+/>
     </div>
   );
 }
